7beedf0f65d69e417e88246a2c3756c9759d182d
Nomeleel <choonlay@outlook.com>
2020-10-10 21:32:45 +0800

    BodyMassIndex.

---
+++ lib/view/body_mass_index/body_mass_index_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'body_mass_index_painter.dart';
+
+class BodyMassIndexView extends StatefulWidget {
+  const BodyMassIndexView({Key key}) : super(key: key);
+
+  @override
+  _BodyMassIndexViewState createState() => _BodyMassIndexViewState();
+}
+
+class _BodyMassIndexViewState extends State<BodyMassIndexView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: Colors.cyan,
+            child: CustomPaint(
+              size: const Size.fromHeight(50),
+              painter: BodyMassIndexPainter(),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.deepOrange,
+          ),
+        )
+      ],
+    );
+  }
+}
