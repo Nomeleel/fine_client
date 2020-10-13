bd835936726db4a771cabd964d5577e61a838c51
Nomeleel <choonlay@outlook.com>
2020-10-13 23:02:33 +0800

    drawHeightPanel

--- lib/view/body_mass_index/body_mass_index_painter.dart
+++ lib/view/body_mass_index/body_mass_index_painter.dart
@@ -63,7 +63,84 @@ class BodyMassIndexPainter extends CustomPainter {
       canvas.rotate(pi * 2 / 120);
     }
 
+    canvas.rotate(-pi * 2 / 120);
+    _drawHeightPanel(canvas);
+
     canvas.restore();
+  }
+
+  void _drawHeightPanel(Canvas canvas) {
+    _drawHeightBackground(canvas);
+
+    final double heightRadius = _painterRadius * 0.6;
+
+    canvas.rotate(pi * 0.75);
+
+    for (int i = 0; i <= 60; i++) {
+      final double height = heightRadius / 27 + (i % 5 == 0 ? 5.0 : 0.0);
+
+      canvas.drawRect(
+        Rect.fromCenter(
+          center: Offset(0, -heightRadius + (height / 2)),
+          width: heightRadius / 195 + (i % 5 == 0 ? 2.0 : 0.0),
+          height: height,
+        ),
+        Paint()..color = _textColor,
+      );
+
+      if (i % 10 == 0) {
+        final TextPainter painter = TextPainter(
+          text: TextSpan(
+            text: '$i',
+            style: TextStyle(
+              color: _textColor,
+              fontSize: heightRadius / 9,
+            ),
+          ),
+          textDirection: TextDirection.ltr,
+        );
+        painter.layout();
+        painter.paint(
+          canvas,
+          Offset(
+            -painter.width / 2,
+            -heightRadius + heightRadius / 7,
+          ),
+        );
+      }
+
+      canvas.rotate(pi * 2 / 120);
+    }
+  }
+
+  void _drawHeightBackground(Canvas canvas) {
+    final double heightPanelRadius = _painterRadius * 0.6;
+    final Rect fullCircleRect = Rect.fromCircle(center: Offset.zero, radius: heightPanelRadius);
+    final ui.Gradient gradient = ui.Gradient.radial(
+      fullCircleRect.center,
+      heightPanelRadius,
+      <Color>[_backgroundHighlightColor, _backgroundColor],
+      const <double>[0, .7],
+    );
+
+    final Path sectorPath = Path.combine(
+      PathOperation.difference,
+      Path()
+        ..addArc(Rect.fromCircle(center: Offset.zero, radius: heightPanelRadius * 0.8), 0, -pi * 0.5)
+        ..lineTo(0, 0),
+      Path()
+        ..addArc(Rect.fromCircle(center: Offset.zero, radius: heightPanelRadius * 0.6), 0, -pi * 0.5)
+        ..lineTo(0, 0),
+    );
+
+    final Path heightPath = Path.combine(PathOperation.difference, Path()..addOval(fullCircleRect), sectorPath);
+
+    canvas.drawPath(
+      heightPath,
+      Paint()
+        ..style = PaintingStyle.fill
+        ..shader = gradient,
+    );
   }
 
   void _drawBackground(Canvas canvas) {
